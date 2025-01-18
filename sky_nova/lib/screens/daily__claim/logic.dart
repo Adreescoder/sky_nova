383f5f4ca2b5d4e9e05da1c1eedaf8e50d9753ab
Adrees White Clean  Coder <adreesking975@gmail.com>
2025-01-18 18:49:22 +0500

    test line

--- sky_nova/lib/screens/daily__claim/logic.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get/get.dart';
-
-import '../wallet/logic.dart';
-
-  class Daily_ClaimLogic extends GetxController {
-  final WalletLogic walletLogic = Get.put(WalletLogic());  // Access WalletLogic here
-  }
--- sky_nova/lib/screens/task_screen/logic.dart
+++
@@ -1,5 +0,0 @@
-import 'package:get/get.dart';
-
-class TaskScreenLogic extends GetxController {
-
-}
