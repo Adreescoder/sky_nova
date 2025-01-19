0e895e646cb41448db0f1035b5f75645911d96b2
Adrees White Clean  Coder <adreesking975@gmail.com>
2025-01-19 17:17:20 +0500

    image picker pir kam kariya ha

--- sky_nova/lib/screens/sigup/view.dart
+++ sky_nova/lib/screens/sigup/view.dart
@@ -3,7 +3,9 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:sky_nova/screens/login/view.dart';
 
 import 'logic.dart';
@@ -15,112 +17,226 @@ class SignupScreen extends StatefulWidget {
 
 class _SignupScreenState extends State<SignupScreen> {
   final SignupLogic logic = Get.put(SignupLogic());
+  bool _isPasswordVisible = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Sign Up"),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          final isSmallScreen = constraints.maxWidth < 600;
+          return Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/1122.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Center(
+              child: SingleChildScrollView(
+                child: Form(
+                  key: logic.formKey,
+                  child: Obx(() {
+                    return Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: isSmallScreen
+                          ? Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: _buildFormContent(isSmallScreen)
+                                  .animate()
+                                  .fadeIn()
+                                  .slideX(
+                                    begin: 1,
+                                    end: 0,
+                                    duration: 800.ms,
+                                    curve: Curves.easeInOut,
+                                  ),
+                            )
+                          : Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  "assets/logo.png",
+                                  height: isSmallScreen ? 150 : 200,
+                                  width: isSmallScreen ? 150 : 200,
+                                ).animate().fadeIn().move(
+                                      duration: GetNumUtils(1).seconds,
+                                      begin: const Offset(-1, 0),
+                                    ),
+                                const SizedBox(width: 40),
+                                SizedBox(
+                                  width: 400,
+                                  child: Column(
+                                    children: _buildFormContent(isSmallScreen)
+                                        .animate()
+                                        .fadeIn(duration: 700.ms),
+                                  ),
+                                ),
+                              ],
+                            ),
+                    );
+                  }),
+                ),
+              ),
+            ),
+          );
+        },
       ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Form(
-              key: logic.formKey,
-              child: Obx(() {
-                return Column(
-                  children: [
-                    GestureDetector(
-                      onTap: () async {
-                        try {
-                          if (kIsWeb) {
-                            // Update UI after image selection
-                          } else if (Platform.isAndroid || Platform.isIOS) {
-                            Get.snackbar(
-                                "Error", "Please Upload Image on Mobile");
-                          }
-                        } catch (e) {
-                          Get.snackbar("Error", "Image picker failed: $e");
-                        }
-                      },
-                      child: Container(
-                        height: 150,
-                        width: 150,
-                        decoration: BoxDecoration(
-                          shape: BoxShape
-                              .circle, // Use BoxShape.circle for better circle
-                          border: Border.all(color: Colors.black),
-                        ),
-                        child: logic.profileImage != null
-                            ? ClipOval(
-                                // ClipOval to ensure circular image
-                                child: Image.memory(logic.profileImage!),
-                              )
-                            : const Icon(CupertinoIcons.camera),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    // Name Field
-                    TextFormField(
-                      controller: logic.userNameController,
-                      decoration: const InputDecoration(
-                        labelText: "Name",
-                        border: OutlineInputBorder(),
-                        prefixIcon: Icon(CupertinoIcons.person),
-                      ),
-                      validator: (value) => value == null || value.isEmpty
-                          ? "Enter your name"
-                          : null,
-                    ),
-                    const SizedBox(height: 20),
-                    // Email Field
-                    TextFormField(
-                      controller: logic.emailController,
-                      decoration: const InputDecoration(
-                        labelText: "Email",
-                        border: OutlineInputBorder(),
-                        prefixIcon: Icon(CupertinoIcons.g),
-                      ),
-                      validator: (value) => value != null &&
-                              !RegExp(r"^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]+")
-                                  .hasMatch(value)
-                          ? "Enter a valid email"
-                          : null,
-                    ),
-                    const SizedBox(height: 20),
-                    // Password Field
-                    TextFormField(
-                      controller: logic.passwordController,
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        labelText: "Password",
-                        border: OutlineInputBorder(),
-                        prefixIcon: Icon(Icons.lock),
-                      ),
-                      validator: (value) => value != null && value.length < 6
-                          ? "Password must be at least 6 characters"
-                          : null,
-                    ),
-                    const SizedBox(height: 20),
-                    // Signup Button
-                    logic.isLoading.value
-                        ? const CircularProgressIndicator()
-                        : ElevatedButton(
-                            onPressed: () => logic.createUser(),
-                            child: const Text("Sign Up"),
-                          ),
-                    const SizedBox(height: 20),
-                    TextButton(
-                      onPressed: () => Get.to(() => LoginPage()),
-                      child: const Text("Already have an account? Log In"),
-                    ),
-                  ],
-                );
-              }),
+    );
+  }
+
+  List<Widget> _buildFormContent(bool isSmallScreen) {
+    return [
+      GestureDetector(
+        onTap: _handleImageUpload,
+        child: Container(
+          height: 150,
+          width: 150,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(color: Colors.black),
+          ),
+          child: logic.profileImage != null
+              ? ClipOval(
+                  child: Image.memory(
+                    logic.profileImage!,
+                    fit: BoxFit.cover,
+                  ),
+                )
+              : Icon(
+                  Icons.camera_alt,
+                  size: 50,
+                  color: Colors.grey,
+                ),
+        ),
+      ),
+      const SizedBox(height: 20),
+      _buildTextField(
+        controller: logic.userNameController,
+        label: "Name",
+        icon: CupertinoIcons.person,
+        validator: (value) => value?.isEmpty == true ? "Enter your name" : null,
+      ),
+      const SizedBox(height: 10),
+      _buildTextField(
+        controller: logic.emailController,
+        label: "Email",
+        icon: CupertinoIcons.mail,
+        validator: (value) => value != null &&
+                !RegExp(r"^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]+").hasMatch(value)
+            ? "Enter a valid email"
+            : null,
+      ),
+      const SizedBox(height: 10),
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: TextFormField(
+          controller: logic.passwordController,
+          obscureText: !_isPasswordVisible,
+          decoration: InputDecoration(
+            labelText: "Password",
+            labelStyle: TextStyle(color: Colors.white),
+            border: const OutlineInputBorder(),
+            prefixIcon: const Icon(CupertinoIcons.lock),
+            suffixIcon: IconButton(
+              icon: Icon(
+                _isPasswordVisible
+                    ? CupertinoIcons.eye_slash
+                    : CupertinoIcons.eye,
+              ),
+              onPressed: () {
+                setState(() {
+                  _isPasswordVisible = !_isPasswordVisible;
+                });
+              },
             ),
           ),
+          validator: (value) => value != null && value.length < 6
+              ? "Password must be at least 6 characters"
+              : null,
         ),
+      ),
+      const SizedBox(height: 20),
+      logic.isLoading.value
+          ? const CircularProgressIndicator()
+          : SizedBox(
+              height: 50,
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () => logic.createUser(),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.purple,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                  ),
+                ),
+                child: const Text(
+                  "Sign Up",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+      const SizedBox(height: 20),
+      TextButton(
+        onPressed: () => Get.to(() => LoginPage()),
+        child: const Text(
+          "Already have an account? Log In",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    ];
+  }
+
+  void _handleImageUpload() async {
+    try {
+      final ImagePicker _picker = ImagePicker();
+      XFile? image;
+
+      if (kIsWeb) {
+        // For web, use image_picker_web
+        image = await _picker.pickImage(source: ImageSource.gallery);
+      } else if (Platform.isAndroid || Platform.isIOS) {
+        // For mobile (Android/iOS), use image_picker
+        image = await _picker.pickImage(source: ImageSource.gallery);
+      }
+
+      if (image != null) {
+        // If an image is picked, convert it to bytes and update the profile image
+        final imageBytes = await image.readAsBytes();
+        setState(() {
+          logic.profileImage = imageBytes;
+        });
+      } else {
+        Get.snackbar("Error", "No image selected.");
+      }
+    } catch (e) {
+      Get.snackbar("Error", "Image picker failed: $e");
+    }
+  }
+
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String label,
+    required IconData icon,
+    String? Function(String?)? validator,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextFormField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: label,
+          labelStyle: TextStyle(color: Colors.white),
+          border: const OutlineInputBorder(),
+          prefixIcon: Icon(icon),
+        ),
+        validator: validator,
       ),
     );
   }
